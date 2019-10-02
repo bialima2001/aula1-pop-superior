71c589c25d7a8f1cdbceb2093a67ff09a8a44946
bialima2001 <45995608+bialima2001@users.noreply.github.com>
2019-10-02 11:03:25 -0300

    Update and rename 598A falta acabar to 598A.cpp

--- 598A.cpp
+++ 598A.cpp
@@ -1,12 +1,12 @@
-
 #include <iostream>
 #include <math.h>
 using namespace std;
  
-long long soma(int a);
+long long soma(long long a);
 
 int main() {
-    int n, temp;
+    int n;
+    long long temp;
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> temp;
@@ -16,14 +16,14 @@ int main() {
     return 0;
 }
 
-long long soma(int a){
+long long soma(long long a){
     long long m;
-    int elevado = 0;
+    long long elevado = 1;
     m = a*(a+1)/2;
-    while(a >= pow(2,elevado)){
-        m-=2*pow(2,elevado);
-        elevado ++;
-        cout << m << "ta no soma" << endl;
+    while(a >= elevado){
+        m-=2*elevado;
+        elevado = elevado*2;
+       // cout << m << "ta no soma" << endl;
     }
     return m;
 }
